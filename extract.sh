8ca9b25b62bcc4f2d8fb8fb5a5d31c08b262ab15
jjungo <j.jungo@gmail.com>
2015-11-24 15:55:33 +0100

    Implement extracting tool

--- extract.sh
+++ extract.sh
@@ -1,11 +1,55 @@
 #!/bin/bash
 
-## Goal:
-#     tar
-#     zip
-#     pzip
-#     tar.gz
-#     tar.xz
-#     7z
-#     tar
-#     gz
+## Supported:
+#   tar
+#   tar.gz
+#   tar.bz2
+#   tar.xz
+#   zip
+#   7z
+
+## Examples:
+# ./extract -h # display this help
+# ./extract <archive> # extract here
+# ./extract <archive> <output direcory> # extract into output directory
+
+echo "**************************"
+echo "* Simple extracting tool *"
+echo "**************************"
+
+## Check arguments
+if [ ! -f $1 ]; then
+    echo File $1 does not exists.
+    exit -1
+fi
+
+if [ ! -z $2 ] && [ ! -f $2 ]; then
+    OUTPUT="$2"
+    mkdir $2
+else
+    OUTPUT="."
+fi
+
+## Exctract
+case "$1" in
+    *.tar )
+        tar xvf $1 -C $2
+        ;;
+    *tar.gz )
+        tar xvzf $1 -C $2
+        ;;
+    *tar.bz2 )
+        tar xvjf $1 -C $2
+        ;;
+    *tar.xz )
+        tar xJvf $1 -C $2
+        ;;
+    *zip )
+        unzip $1 -d $2
+        ;;
+    *7z )
+        7z x $1 -o$2
+        ;;
+    *)
+        ;;
+esac
